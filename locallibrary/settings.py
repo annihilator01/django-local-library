443e315ade24253571ebfe563a2bfbb4963dbff4
Ilia <just.strange.man@gmail.com>
2020-08-19 03:11:59 +0300

    Update DEBUG

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['polar-mountain-73619.herokuapp.com', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
